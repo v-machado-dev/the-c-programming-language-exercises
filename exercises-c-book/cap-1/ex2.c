3152c7a86c3df80d8769a12faa3d7f34d28c6dd8
Victor Machado <vprograms21@gmail.com>
2026-05-07 14:39:11 +0000

    gitignore and first 6 exercises

---
+++ exercises-c-book/cap-1/ex2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+    char* x = "Cleiton";
+    printf("Hello, %s\n", x);
+}
+
---
+++ exercises-c-book/cap-1/ex5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int c;
+    int nl = 0;
+    int tab  = 0;
+    int s = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n'){
+            nl++;
+        
+        } else if (c == '\t'){
+            tab++;
+        
+        } else if (c == ' '){
+            s++;
+        }        
+    }
+    printf("'tab:'%d 'nl:'%d 's:'%d\n", tab, nl, s);
+}
+
