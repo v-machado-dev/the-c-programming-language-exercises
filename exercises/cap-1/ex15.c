2ab1dab53127cbfc999204bfef010b4395199800
v-machado-dev <vprograms21@gmail.com>
2026-05-12 19:40:57 -0300

    exercise 15

---
+++ exercises/cap-1/ex15.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define MAX 300
+#define MIN 0
+
+// rewrites the temperature conversion program of Section 1.2 to use a function
+// for the conversion.
+
+float calculate(float fahr, float celsius, int var, float lower, float upper);
+
+int main(){
+
+    float fahr, celsius;
+    float lower, upper;
+    int var;
+  
+    calculate(fahr, celsius,var,lower,upper);
+   
+    printf("Done!\n");
+}
+
+float calculate(float fahr, float celsius, int var, float lower, float upper){
+    
+    lower = MIN;
+    upper = MAX;
+    var = 20;
+    fahr = lower;
+
+    printf("Fahrenheit | Celsius\n");
+
+    while(fahr <= upper){
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%f  %f\n", fahr, celsius);
+        fahr = fahr + var;
+    }return fahr;
+}
