2fefb6aab4606a44cae5fddc7999e6c2b7287183
v-machado-dev <vprograms21@gmail.com>
2026-05-12 11:46:14 -0300

    exercise 13

--- exercises/cap-1/ex12.c
+++ exercises/cap-1/ex12.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
+
+#define IN  1
 #define OUT 0
-#define IN 1
 
-int main(){
+// prints out a new line for every new word
 
-    int state = OUT;
-    int c;
-    while((c = getchar()) != EOF){
+int main() {
 
-        if((c == ' ' || c == '\t') && state == IN){
-            c = '\n';
+    int c, state;
+    state = OUT;
+
+    // prints a new line for every blank, tab or new line
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t'){
+            if (state == IN){
+                putchar('\n');
+            }
             state = OUT;
-            putchar(c);
         
-        } else if (state == OUT && (c == ' ' || c == '\t')){
-            
-            continue;
-        
+        // if last c was a letter then print the current one 
+
         } else {
-            
             state = IN;
             putchar(c);
         }
     }
+    return 0;
 }
---
+++ exercises/cap-1/ex13.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+// program that prints an histogram of each frequency of input's words's lenghts
+
+int main(){
+
+    // declaring variables and populating array
+
+    int len[100];
+    int c, i,j;
+    int size = 0;
+    int state = IN;
+
+    for (i = 0; i < 100; i++){
+        len[i] = 0;
+    }
+
+    while((c = getchar()) != EOF){
+
+        // increases the count for inputs's word's lenghts and restarts size
+
+        if((c == ' ' || c == '\n' || c == '\t') && state == IN){   
+            len[size]++;
+            state = OUT;
+            size = 0;
+        }
+
+        // ignores extra spaces
+
+        else if((c == ' ' || c == '\n' || c == '\t') && state == OUT){
+            continue;
+        }
+
+        // if c is not a word separator increase word size
+
+        else{
+            state = IN;
+            size++;
+        }
+    }
+
+    // the below operation must be done if after the loop size !=0
+    // because it means that there isn't a word separator between
+    // said digit and EOF, which breaks the code 
+
+    if (size != 0){
+        len[size]++;
+        size = 0;
+        printf("\n");
+    }
+
+    // nested for loops for printing accordingly 
+
+    for (i = 0; i < 100; i++){
+
+        if (len[i] > 0){
+            printf(" %d :", i);
+
+            for (j = 0; j < len[i]; j++){        
+                  printf(" #");
+            }
+            printf("\n");
+        }
+    }
+}
